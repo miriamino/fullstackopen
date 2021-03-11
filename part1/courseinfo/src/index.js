ff991ae306a38500e072a2417b757a0be024416f
miriamino <29210881+miriamino@users.noreply.github.com>
2021-03-11 12:44:49 +0100

    refactor components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,17 +5,15 @@ const Header = (props) =>
   <h1>{props.course.name}</h1>
 
 const Part = (props) =>
-    <p>{props.part.name} {props.part.exercises}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
 
 const Content = (props) =>
-  <>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />   
-    <Part part={props.parts[2]} />
-  </>
+  <ul>
+    {props.parts.map((part) => <li><Part part={part} /></li>)}
+  </ul>
 
 const Total = (props) =>
-  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  <p>Number of exercises {props.parts.reduce((a, b) => a + b.exercises, 0)}</p>
 
 
 const App = () => {
